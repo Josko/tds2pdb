da8c483ad5acb926a617fb3824a83c41375d0bf6
Josko <josko.nikolic1@gmail.com>
2017-02-02 22:34:26 +0100

    Initial commit - CMake, main and GTest. Additional things include gitignore and ycm config file.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "tds2pdb.hpp"
+
+#include <cstdint>
+
+int32_t main(const int32_t argc, const char** argv)
+{
+  return 0;
+}
---
+++ test/Test1.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(Loading, Initial) {
+  EXPECT_EQ(1, 1);
+}
