c090da887b30e7fa3a08dc00e7bb05b6c1792002
Josko <josko.nikolic1@gmail.com>
2017-02-19 10:48:00 +0100

    Continue the implementation of the TDS parsing functions.

--- src/tds_parser.cpp
+++ src/tds_parser.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+/// @brief Parses out the available information out of the TDS file.
+///
+/// @param tds_file TDS file to be processed.
+///
+/// @return Object will all of the data stored.
+///
+/// @throw std::runtime_error in the event of an parsing error.
 TdsFormat parse_tds(const fs::path tds_file)
 {
   TdsFormat tds_format;
@@ -14,11 +21,20 @@ TdsFormat parse_tds(const fs::path tds_file)
     const auto signature = read_string<4>(tds());
     if (signature != "FB0A")
       throw std::runtime_error("Wrong signature found! [" + signature + "]");
+
+    const auto offset = read_numeric<int32_t>(tds());
+    tds().seekg(offset);
+
+    const auto dir_header_size = read_numeric<int16_t>(tds());
+    assert(dir_header_size == 16);
+
+    const auto dir_entry_size = read_numeric<int16_t>(tds());
+    assert(dir_entry_size == 12);
   }
   catch (const std::runtime_error& e)
   {
     std::cout << "Failed parsing the TDS file: " << e.what() << '\n';
-    return tds_format;
+    throw std::runtime_error(e.what());
   }
 
   return tds_format;
